00edc5c97404225750549e4e1f7fad2217ac8371
AhmadAmerBakran <114908029+AhmadAmerBakran@users.noreply.github.com>
2024-03-12 09:44:42 +0100

    refresh method has been added

--- lib/weekly_forecast_screen.dart
+++ lib/weekly_forecast_screen.dart
@@ -1,22 +1,45 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather/weather_sliver_app_bar.dart';
 import 'package:weather/weekly_forecast_list.dart';
 
 import 'data_source.dart';
+import 'models.dart';
 
-class WeeklyForecastScreen extends StatelessWidget {
+class WeeklyForecastScreen extends StatefulWidget {
   const WeeklyForecastScreen({super.key});
 
   @override
+  _WeeklyForecastScreenState createState() => _WeeklyForecastScreenState();
+}
+
+class _WeeklyForecastScreenState extends State<WeeklyForecastScreen> {
+
+  final controller = StreamController<WeeklyForecastDto>();
+
+  @override
+  void initState() {
+    super.initState();
+    loadForecast();
+  }
+
+  Future<void> loadForecast() async {
+    final future = context.read<DataSource>().getWeeklyForecast();
+    controller.addStream(future.asStream());
+    print('updating ....');
+    await future;
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: context.read<DataSource>().getWeeklyForecast(),
+      body: StreamBuilder(
+        stream: controller.stream,
         builder: (context, snapshot) {
           return CustomScrollView(
             slivers: <Widget>[
-              const WeatherSliverAppBar(),
+              WeatherSliverAppBar(onRefresh: loadForecast),
               if (snapshot.hasData)
                 WeeklyForecastList(weeklyForecast: snapshot.data!)
               else if (snapshot.hasError)
